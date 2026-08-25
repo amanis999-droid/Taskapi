ec9b790c53a828e72c9d136c4df598188a466d34
amanis999-droid <fr54aman@gmail.com>
2026-08-25 23:27:55 +0530

    project setup with health check endpoint

---
+++ src/main/java/com/TaskAPI/Taskapi/Controller/HealthController.java
@@ -0,0 +1,11 @@
+package com.TaskAPI.Taskapi.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HealthController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
